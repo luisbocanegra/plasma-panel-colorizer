7477eb292c6ab6bb95c21b1a23292ebd940bf936
Luis Bocanegra <luisbocanegra@users.noreply.github.com>
2024-03-06 00:55:06 -0600

    Update build config

--- install.sh
+++ install.sh
@@ -1,16 +1,9 @@
 #!/bin/sh
-
-# Remove the build directory if it exists
 if [ -d "build" ]; then
     rm -rf build
 fi
 
-
 # install plasmoid only
-cmake -B build -S . -DBUILD_PLUGIN=OFF -DCMAKE_INSTALL_PREFIX=~/.local
-
-# Build the project
+cmake -B build -S . -DCMAKE_INSTALL_PREFIX=~/.local
 cmake --build build
-
-# Install the built project
 cmake --install build
--- package-plasmoid.sh
+++ package-plasmoid.sh
@@ -1,12 +1,9 @@
 #!/bin/sh
 
-# Remove the build directory if it exists
 if [ -d "build" ]; then
     rm -rf build
 fi
 
-# skip building/installing
+# package plasmoid, skip installing
 cmake -B build -S . -DINSTALL_PLASMOID=OFF -DPACKAGE_PLASMOID=ON
-
-# package plasmoid
 cmake --build build
