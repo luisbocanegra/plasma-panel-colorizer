e43074d35a6bb7de46e016f47a3298527bbefdf6
Luis Bocanegra <luisbocanegra@users.noreply.github.com>
2024-06-06 13:27:27 -0600

    System-wide install only for plugin

--- install.sh
+++ install.sh
@@ -4,6 +4,12 @@ if [ -d "build" ]; then
     rm -rf build
 fi
 
-cmake -B build -S . -DINSTALL_PLASMOID=ON -DBUILD_PLUGIN=ON -DCMAKE_INSTALL_PREFIX=/usr
-cmake --build build
-sudo cmake --install build
+# Install widget for current user
+cmake -B build/plasmoid -S . -DINSTALL_PLASMOID=ON -DCMAKE_INSTALL_PREFIX=~/.local
+cmake --build build/plasmoid
+cmake --install build/plasmoid
+
+# Install plugin system-wide (required for qml modules)
+cmake -B build/plugin -S . -DBUILD_PLUGIN=ON -DCMAKE_INSTALL_PREFIX=/usr
+cmake --build build/plugin
+sudo cmake --install build/plugin
