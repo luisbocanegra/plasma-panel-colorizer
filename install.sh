2d831924803240cc242a9a7d8797054a234b9652
Luis Bocanegra <luisbocanegra@users.noreply.github.com>
2024-12-17 19:48:24 -0600

    set list_presets.sh executable permission for manual install.sh script
    closes: https://github.com/luisbocanegra/plasma-panel-colorizer/issues/129

--- install.sh
+++ install.sh
@@ -8,6 +8,8 @@ fi
 cmake -B build/plasmoid -S . -DINSTALL_PLASMOID=ON -DCMAKE_INSTALL_PREFIX=~/.local
 cmake --build build/plasmoid
 cmake --install build/plasmoid
+# CMakeLists.txt plasma_install_package does't copy executable permission
+chmod 700 "$HOME/.local/share/plasma/plasmoids/luisbocanegra.panel.colorizer/contents/ui/tools/list_presets.sh"
 
 # Install plugin system-wide (required for qml modules)
 cmake -B build/plugin -S . -DBUILD_PLUGIN=ON -DCMAKE_INSTALL_PREFIX=/usr
