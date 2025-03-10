a588828f0ac4a6fe3bfc3faf8e7667d869912c75
Luis Bocanegra <luisbocanegra@users.noreply.github.com>
2025-03-09 23:33:11 -0600

    build: change plugin install from system-wide to widget directory
    Now the install scripts can be used on immutable distributions
    
    refs: https://github.com/luisbocanegra/plasma-panel-colorizer/issues/194

--- install-plugin.sh
+++ install-plugin.sh
@@ -4,6 +4,5 @@ if [ -d "build" ]; then
     rm -rf build
 fi
 
-# Install plugin system-wide (required for qml modules)
-cmake -B build/plugin -S . -DBUILD_PLUGIN=ON -DCMAKE_INSTALL_PREFIX=/usr
-cmake --build build/plugin && sudo cmake --install build/plugin
+cmake -B build/plugin -S . -DBUILD_PLUGIN=ON -DCMAKE_INSTALL_PREFIX=~/.local
+cmake --build build/plugin && cmake --install build/plugin
--- install.sh
+++ install.sh
@@ -5,13 +5,8 @@ if [ -d "build" ]; then
 fi
 
 # Install widget for current user
-cmake -B build/plasmoid -S . -DINSTALL_PLASMOID=ON -DCMAKE_INSTALL_PREFIX=~/.local
-cmake --build build/plasmoid
-cmake --install build/plasmoid
+cmake -B build/ -S . -DINSTALL_PLASMOID=ON -DBUILD_PLUGIN=ON -DCMAKE_INSTALL_PREFIX=~/.local
+cmake --build build/
+cmake --install build/
 # CMakeLists.txt plasma_install_package does't copy executable permission
 chmod 700 "$HOME/.local/share/plasma/plasmoids/luisbocanegra.panel.colorizer/contents/ui/tools/list_presets.sh"
-
-# Install plugin system-wide (required for qml modules)
-cmake -B build/plugin -S . -DBUILD_PLUGIN=ON -DCMAKE_INSTALL_PREFIX=/usr
-cmake --build build/plugin
-sudo cmake --install build/plugin
