6bd8c23ac050d32a1150df727bcfcd26c087b8c0
Luis Bocanegra <luisbocanegra@users.noreply.github.com>
2025-03-14 18:05:06 -0600

    revert: "build: change plugin install from system-wide to widget directory"
    It wasn't a good approach because updates to the widget remove the plugin and it needs to be installed again.
    
    Support for immutable distributions using QML_IMPORT_PATH will be added in later patch
    
    refs: a588828f0ac4a6fe3bfc3faf8e7667d869912c75

--- install-plugin.sh
+++ install-plugin.sh
@@ -4,5 +4,6 @@ if [ -d "build" ]; then
     rm -rf build
 fi
 
-cmake -B build/plugin -S . -DBUILD_PLUGIN=ON -DCMAKE_INSTALL_PREFIX=~/.local
-cmake --build build/plugin && cmake --install build/plugin
+# Install plugin system-wide (required for qml modules)
+cmake -B build/plugin -S . -DBUILD_PLUGIN=ON -DCMAKE_INSTALL_PREFIX=/usr
+cmake --build build/plugin && sudo cmake --install build/plugin
--- install.sh
+++ install.sh
@@ -5,9 +5,14 @@ if [ -d "build" ]; then
 fi
 
 # Install widget for current user
-cmake -B build/ -S . -DINSTALL_PLASMOID=ON -DBUILD_PLUGIN=ON -DCMAKE_INSTALL_PREFIX=~/.local
-cmake --build build/
-cmake --install build/
+cmake -B build/plasmoid -S . -DINSTALL_PLASMOID=ON -DCMAKE_INSTALL_PREFIX=~/.local
+cmake --build build/plasmoid
+cmake --install build/plasmoid
 # CMakeLists.txt plasma_install_package does't copy executable permission
 chmod 700 "$HOME/.local/share/plasma/plasmoids/luisbocanegra.panel.colorizer/contents/ui/tools/list_presets.sh"
 chmod 700 "$HOME/.local/share/plasma/plasmoids/luisbocanegra.panel.colorizer/contents/ui/tools/gdbus_get_signal.sh"
+
+# Install plugin system-wide (required for qml modules)
+cmake -B build/plugin -S . -DBUILD_PLUGIN=ON -DCMAKE_INSTALL_PREFIX=/usr
+cmake --build build/plugin
+sudo cmake --install build/plugin
