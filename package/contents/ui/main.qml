luisbocanegra/plasma-panel-colorizer

commit b81b371b99cbc0ce90154e9490e5ccb41b7370bd

feat: option to remove 'Configure Panel Colorizer' option from other widget's context menu
refs: https://github.com/luisbocanegra/plasma-panel-colorizer/issues/388

package/contents/ui/main.qml
@@ -60,6 +60,7 @@ PlasmoidItem {
     property bool nativePanelBackgroundEnabled: (isEnabled ? cfg.nativePanel.background.enabled : true) || doPanelClickFix
     property real nativePanelBackgroundOpacity: isEnabled ? cfg.nativePanel.background.opacity : 1.0
     property bool nativePanelBackgroundShadowEnabled: isEnabled ? cfg.nativePanel.background.shadow : true
+    property bool configureFromAllWidgets: Plasmoid.configuration.configureFromAllWidgets
     property var panelWidgets: []
     property int panelWidgetsCount: 0
     property real trayItemThikness: 20
@@ -1607,11 +1608,44 @@ PlasmoidItem {
         panelWidgetsCount = panelWidgets.length;
     }
 
-    PlasmaCore.Action {
-        id: configureAction
-        text: plasmoid.internalAction("configure").text
-        icon.name: 'configure'
-        onTriggered: plasmoid.internalAction("configure").trigger()
+    Component {
+        id: configureActionComponent
+        PlasmaCore.Action {
+            text: plasmoid.internalAction("configure").text
+            objectName: "panelColorizerConfigureAction"
+            icon.name: 'configure'
+            onTriggered: plasmoid.internalAction("configure").trigger()
+        }
+    }
+
+    onConfigureFromAllWidgetsChanged: {
+        updateContextualActions(configureFromAllWidgets);
+    }
+
+    function updateContextualActions(enabled) {
+        if (!main.panelLayout)
+            return;
+        for (var i in main.panelLayout.children) {
+            const child = main.panelLayout.children[i];
+            // may not be available while dragging into the panel and other situations
+            if (!child.applet?.plasmoid?.pluginName)
+                continue;
+
+            if (child.applet.plasmoid.pluginName === Plasmoid.metaData.pluginId) {
+                continue;
+            }
+            child.applet.plasmoid.contextualActions = child.applet.plasmoid.contextualActions.filter(item => {
+                if (item && item.objectName === "panelColorizerConfigureAction") {
+                    item.destroy();
+                    return false;
+                }
+                return true;
+            });
+            if (enabled) {
+                const action = configureActionComponent.createObject(main);
+                child.applet.plasmoid.contextualActions.push(action);
+            }
+        }
     }
 
     function showPanelBg(panelBg) {
@@ -1649,6 +1683,9 @@ PlasmoidItem {
         }
         Utils.delay(100, () => {
             applyStockPanelSettings();
+        }, main);
+        Utils.delay(500, () => {
+            updateContextualActions(configureFromAllWidgets);
         }, main);
     }
 

package/contents/config/main.xml
@@ -97,5 +97,10 @@
         type="Bool">
         <default>false</default>
     </entry>
+    <entry
+        name="configureFromAllWidgets"
+        type="Bool">
+        <default>false</default>
+    </entry>
   </group>
 </kcfg>

package/contents/ui/configGeneral.qml
@@ -17,6 +17,7 @@ KCM.SimpleKCM {
     property alias cfg_pythonExecutable: pythonExecutable.text
     property alias cfg_animatePropertyChanges: animatePropertyChanges.checked
     property alias cfg_animationDuration: animationDuration.value
+    property alias cfg_configureFromAllWidgets: configureFromAllWidgets.checked
     property string cfg_editModeGridSettings
     property var editModeGridSettings: JSON.parse(cfg_editModeGridSettings)
     property string presetsDir: StandardPaths.writableLocation(StandardPaths.HomeLocation).toString().substring(7) + "/.config/panel-colorizer/presets"
@@ -37,7 +38,17 @@ KCM.SimpleKCM {
                 Kirigami.FormData.label: i18n("Hide widget:")
                 checked: cfg_hideWidget
                 onCheckedChanged: cfg_hideWidget = checked
-                text: i18n("visible in Panel Edit Mode")
+                text: i18n("Visible in Panel Edit Mode")
+            }
+
+            RowLayout {
+                Kirigami.FormData.label: i18n("Configure from any widget:")
+                CheckBox {
+                    id: configureFromAllWidgets
+                }
+                Kirigami.ContextualHelpButton {
+                    toolTipText: i18n("Disable to remove <b>%1</b> option from other widget's context (right-click) menu.", Plasmoid.internalAction("configure").text)
+                }
             }
 
             CheckBox {

commit 181bba53b48d219e1b71619cf858d31ad0a697b7

fix: configureAction missing when a widget is added to panel
fixes regression introduced by b81b371

package/contents/ui/main.qml
@@ -1482,6 +1482,7 @@ PlasmoidItem {
             Utils.showWidgets(panelLayout, backgroundComponent, Plasmoid);
             updateCurrentWidgets();
             showPanelBg(panelBg);
+            updateContextualActions(configureFromAllWidgets);
         });
     }
 